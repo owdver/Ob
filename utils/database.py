72023c286e7779f22310288d079f271bf3d6654b
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2024-07-08 02:40:49 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import DATABASE_URI, DATABASE_NAME
+import logging
 
-client = AsyncIOMotorClient(DATABASE_URI)
-db = client[DATABASE_NAME]
+logger = logging.getLogger(__name__)
+
+try:
+    client = AsyncIOMotorClient(DATABASE_URI)
+    db = client[DATABASE_NAME]
+    logger.info("Connected to the database successfully.")
+except Exception as e:
+    logger.error(f"Failed to connect to the database: {e}")
